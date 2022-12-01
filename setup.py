6b9035b9922ec87a0b09082f28605c1c03449d6b
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2022-12-01 12:23:20 -0500

    Chnaged to pyproject.toml specification

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-import re
-from setuptools import setup, find_packages
+import setuptools
 
-with open("animeflv/__init__.py", "r") as fd:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
-    ).group(1)
-
-with open("requirements.txt", "r") as file:
-    INSTALL_REQUIRES = file.readlines()
-
-with open("README.md", "r") as readme:
-    setup(
-        name="animeflv",
-        version=version,
-        description="AnimeFLV is a python custom API for https://animeflv.net website",
-        long_description=readme.read(),
-        long_description_content_type="text/markdown",
-        license="MIT License",
-        author="Jorge Alejandro Jiménez Luna",
-        author_email="jorgeajimenezl17@gmail.com",
-        url="https://github.com/jorgeajimenezl/animeflv-api",
-        classifiers=[
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
-        ],
-        keywords="animeflv, anime, videos, manga",
-        install_requires=INSTALL_REQUIRES,
-        packages=find_packages(),
-    )
+if __name__ == "__main__":
+    setuptools.setup(name="animeflv")
